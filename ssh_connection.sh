be8470712257c9769c1a5832ec8f6606ce4765c8
zhuyj <zyjzyj2000@gmail.com>
2015-11-23 22:05:03 -0500

    get local ip address

--- ssh_connection.sh
+++ ssh_connection.sh
@@ -2,10 +2,12 @@
 SSH_EXISTS=`ps uxww | grep 140:5656 | grep -v grep`
 if [ -z "${SSH_EXISTS}" ]; then
 	echo "ssh tunnel error!"
+	IPADDRESS=`ip -4 addr list eth0 | grep inet | awk -F " " '{print $2}' | awk -F "/" '{print $1}'`
+	echo ${IPADDRESS}
 	date
 	expect -c "  
 		set timeout 10
-		spawn ssh -D 128.224.162.140:5656 -qgTfnN yzhu1@ala-lpd-susbld2.wrs.com  
+		spawn ssh -D ${IPADDRESS}:5656 -qgTfnN yzhu1@ala-lpd-susbld2.wrs.com  
 		expect \"password:\"
 		send \"password!\r\"
 		expect eof
